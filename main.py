9695f8b906fe00130611ef0473dac14a807b7fb0
Ahsokastar <catherine.todd@gmail.com>
2025-05-07 17:17:54 -0500

    e

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def load_data():
 
 data = load_data()
 fail_hydepark = data[(data['Results'] == 'Fail') & (data["Zip"] == 60615)]
-rates = data.groupby('Results').size() / data.shape[0]
+rates = (data.groupby('Results').size() / data.shape[0] ) * 100
 st.write("hello")
 st.write(rates)
-st.write(fail.head(50))
+st.write(fail_hydepark.head(50))
