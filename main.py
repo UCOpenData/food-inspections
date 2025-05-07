efcdbf8c0c8b75b3f5ffa08ba0d8bb8a110ee2cf
Ahsokastar <catherine.todd@gmail.com>
2025-05-07 17:45:25 -0500

    s

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def load_data():
     return df
 
 data = load_data()
-fail_hydepark = data[(data['Results'] == 'Fail') & (data["Zip"] == 60615) | (data["Zip"] == 60637)]
+fail_hydepark = data[(data['Results'] == 'Fail') & ((data["Zip"] == 60615) | (data["Zip"] == 60637))]
 rates = (data.groupby('Results').size().to_frame('Percentage') / data.shape[0] ) * 100
 st.write("hello")
 st.write(rates)
